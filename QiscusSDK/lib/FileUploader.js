821e278eff822f2fe15f86570e787b2190cf54ee
evanpurnama <evanpurnama5@gmail.com>
2018-03-20 15:58:40 +0700

    Update FileUploader.js

--- QiscusSDK/lib/FileUploader.js
+++ QiscusSDK/lib/FileUploader.js
@@ -9,9 +9,11 @@ import autobind from 'class-autobind';
 import styles from './styles';
 
 const CANCEL_INDEX = 0;
-const DESTRUCTIVE_INDEX = 2;
-const options = [ 'Cancel', 'Image' ];
+const DESTRUCTIVE_INDEX = 3;
+const options = { title: "Upload Image"};
+const optionsArray = [ 'Cancel', 'Choose from Library', "Take a Photo" ];
 const title = 'File type?';
+var alreadyCalled = false;
 
 export default class FilePicker extends Component {
   constructor() {
@@ -24,13 +26,15 @@ export default class FilePicker extends Component {
     autobind(this);
   }
   _unSelect() {
+    alreadyCalled = false;
     this.setState({
       selected: null,
     });
   }
   _pickImage() {
+
     let {props: {sendMessage, setSending}} = this;
-    ImagePicker.showImagePicker(options, (response) => {
+    ImagePicker.launchImageLibrary(options, (response) => {
       if (response.didCancel) {
         this._unSelect();
         setSending(false);
@@ -69,6 +73,51 @@ export default class FilePicker extends Component {
       }
     });
   }
+  _pickImageCamera() {
+
+    let {props: {sendMessage, setSending}} = this;
+    ImagePicker.launchCamera(options, (response) => {
+      if (response.didCancel) {
+        this._unSelect();
+        setSending(false);
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        this._unSelect();
+        setSending(false);
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        this._unSelect();
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        this._unSelect();
+        setSending(true);
+        let source = Platform.OS === 'ios' ? response.uri : response.path;
+        console.log("SOURCE", source);
+        console.log("response", response);
+        const settings = {
+          uri: source,
+          uploadUrl: `https://sdksample.qiscus.com/api/v2/sdk/upload`,
+          method: 'POST', // default to 'POST'
+          fileName: "image-"+ Date.now().toString() + ".jpg",
+          fieldName: 'file',
+          contentType: `multipart/form-data`, // default to 'application/octet-stream'
+          data: {
+            token: this.state.token,
+          }
+        };
+        FileUploader.upload(settings, (err, res) => {
+          const data = JSON.parse(res.data);
+          console.log("data", data)
+          sendMessage(`[file] ${data.results.file.url} [/file]`);
+        }, (sent, expectedToSend) => {
+            // do something when uploading
+        });
+      }
+    });
+  }  
   _handlePress(i) {
     this.setState({
       selected: i
@@ -83,8 +132,13 @@ export default class FilePicker extends Component {
         attachIconStyle
       },
       state: {selected}} = this;
-    if (selected === 1) {
+    if (selected === 1 && alreadyCalled == false) {
       this._pickImage();
+      alreadyCalled = true;
+    }
+    if (selected === 2 && alreadyCalled == false) {
+      this._pickImageCamera();
+      alreadyCalled = true;
     }
     return (
       <View>
@@ -94,7 +148,7 @@ export default class FilePicker extends Component {
         <ActionSheet
           ref={o => this.ActionSheet = o}
           title={title}
-          options={options}
+          options={optionsArray}
           cancelButtonIndex={CANCEL_INDEX}
           destructiveButtonIndex={DESTRUCTIVE_INDEX}
           onPress={this._handlePress}
